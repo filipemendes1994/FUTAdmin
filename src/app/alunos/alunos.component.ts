f4180038a1a12eaf513b48ec66f000017db14ed8
danielgek <danielgek@gmail.com>
2016-07-20 16:59:29 +0100

    add alunos and professores components

---
+++ src/app/alunos/alunos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-alunos',
+  templateUrl: 'alunos.component.html',
+  styleUrls: ['alunos.component.css']
+})
+export class AlunosComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
--- src/system-config.ts
+++ src/system-config.ts
@@ -40,6 +40,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/home',
+  'app/alunos',
   /** @cli-barrel */
 ];
 
