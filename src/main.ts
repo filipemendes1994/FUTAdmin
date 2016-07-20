bdb99c3d4fbe20fc7563ffcd06e7ea340646d313
Filipe Mendes <filipemendes1994@gmail.com>
2016-07-20 01:16:59 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "AIzaSyDgcTBKLLDiYciCsj8UUeWUfGGeMEs-sKM",
+    authDomain: "futadmin-ea505.firebaseapp.com",
+    databaseURL: "https://futadmin-ea505.firebaseio.com",
+    storageBucket: "futadmin-ea505.appspot.com",
+  })
 
+]);
