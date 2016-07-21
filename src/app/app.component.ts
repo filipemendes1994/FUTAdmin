1ba543aac48d7e29f5c62d5ce5cd88a3a71658ff
Filipe Mendes <filipemendes1994@gmail.com>
2016-07-21 20:22:34 +0100

    toolbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import {MD_ICON_DIRECTIVES, MdIconRegistry} from '@angular2-material/icon';
+import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
+import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  directives: [ROUTER_DIRECTIVES, MD_ICON_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, MD_ICON_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES],
   providers: [MdIconRegistry],
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
 })
+
 export class AppComponent implements OnInit {
   title = 'app works!';
   items: FirebaseListObservable<any[]>;
