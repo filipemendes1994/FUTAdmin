f95cf1189bdef311c1624bc3a86f27a0ace2425e
danielgek <danielgek@gmail.com>
2016-07-22 14:20:44 +0100

    add side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,25 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 import {MD_ICON_DIRECTIVES, MdIconRegistry} from '@angular2-material/icon';
 import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
 import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  directives: [ROUTER_DIRECTIVES, MD_ICON_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES],
+  directives: [
+    ROUTER_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MD_ICON_DIRECTIVES,
+    MD_TOOLBAR_DIRECTIVES,
+    MD_BUTTON_DIRECTIVES,
+    MD_LIST_DIRECTIVES
+  ],
   providers: [MdIconRegistry],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
-
 export class AppComponent implements OnInit {
   title = 'app works!';
   items: FirebaseListObservable<any[]>;
