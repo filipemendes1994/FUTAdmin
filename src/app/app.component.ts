1d863efc11089314f6d29961fadb5aace13f3d02
danielgek <danielgek@gmail.com>
2016-07-24 21:21:29 +0100

    clean app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import {MD_ICON_DIRECTIVES, MdIconRegistry} from '@angular2-material/icon';
 import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
 import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import { AuthService } from './auth.service';
 
 
 
@@ -28,10 +28,9 @@ import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
-  items: FirebaseListObservable<any[]>;
+  
 
-  constructor(public af: AngularFire) { }
+  constructor(af:AuthService) {}
 
   ngOnInit() {
 
