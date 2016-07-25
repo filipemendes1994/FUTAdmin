48ad145805b7c5fa91ad076c961e9c2105770a64
Filipe Mendes <filipemendes1994@gmail.com>
2016-07-25 02:45:15 +0100

    edit alunos done

--- src/app/alunos/list/list-alunos.component.ts
+++ src/app/alunos/list/list-alunos.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
-import { Aluno } from '../aluno';
+import { IAluno, Aluno } from '../aluno';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { AlunosService } from '../alunos.service';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { ROUTER_DIRECTIVES,  Router} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'list-alunos',
-  directives: [ MD_LIST_DIRECTIVES, ROUTER_DIRECTIVES ],
+  directives: [ MD_LIST_DIRECTIVES, ROUTER_DIRECTIVES],
   templateUrl: 'list-alunos.component.html',
   styleUrls: ['list-alunos.component.css']
 })
 export class ListAlunosComponent implements OnInit {
 
-  alunos: FirebaseListObservable<Aluno[]>;
-  constructor(public as: AlunosService) {}
+  alunos: FirebaseListObservable<IAluno[]>;t
+  constructor(public as: AlunosService, private router: Router) {}
 
   ngOnInit() {
     this.alunos = this.as.getAlunos();
   }
 
+  goToAluno(key: string)
+  {
+    this.router.navigate(['/alunos', key]);
+  }
 }
