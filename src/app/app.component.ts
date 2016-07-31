1fb5f44c30d12982b20280b880b1909a01199a49
danielgek <danielgek@gmail.com>
2016-07-31 23:26:14 +0100

    add back button to toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
-import {MD_ICON_DIRECTIVES, MdIconRegistry} from '@angular2-material/icon';
-import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
-import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
-import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { ROUTER_DIRECTIVES, ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MD_SIDENAV_DIRECTIVES, MdSidenav } from '@angular2-material/sidenav';
 import { AuthService } from './auth.service';
 
 
@@ -25,14 +26,33 @@ import { AuthService } from './auth.service';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   pipes: [],
-  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
+  showBack: boolean = false;
+  showSidenav: boolean = false;
+  @ViewChild(MdSidenav) sidenav:MdSidenav;
 
-  constructor() {}
+  constructor( private router: Router, private location: Location, public as:AuthService) {}
 
   ngOnInit() {
+    this.router.events.subscribe(path => {
+      if (this.as.isLogged){
+        this.sidenav.open();
+      }else{
+        this.sidenav.close();
+      }
+      // if the path is 2 steps deep
+      if (path.url.split('/').length - 1 >= 2) {
+        this.showBack = true;
+      }else{
+        this.showBack = false;
+      }
+    });
 
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
 
